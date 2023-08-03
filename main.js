5bbd4347f410f911a5b48fd1d4f01342f89d9c28
HimanshuMukane <88427956+HimanshuMukane@users.noreply.github.com>
2023-08-03 21:47:32 +0530

    3d test 1

--- main.js
+++ main.js
@@ -1,66 +1,73 @@
-const canvas = document.getElementById('canvas');
-const permissionButton = document.getElementById('permissionButton');
-let gimbal;
+let camera, scene, renderer;
+let gltfModel;
 
 function init() {
-    // Set up the renderer and scene
-    const renderer = new THREE.WebGLRenderer({ canvas });
-    renderer.setSize(window.innerWidth, window.innerHeight);
+  scene = new THREE.Scene();
 
-    const scene = new THREE.Scene();
+  // Create a fixed camera at a position
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.set(0, 0, 5);
+  scene.add(camera);
 
-    // Set up the camera
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(0, 0, 5);
-    scene.add(camera);
+  // Add lighting to the scene
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+  scene.add(ambientLight);
 
-    // Load the GLTF cube model
-    const loader = new THREE.GLTFLoader();
-    loader.load('files/Home.gltf', (gltf) => {
-        scene.add(gltf.scene);
-    });
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+  directionalLight.position.set(0, 1, 0);
+  scene.add(directionalLight);
 
-    // Set up the gimbal
-    gimbal = new Gimbal();
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.getElementById('container').appendChild(renderer.domElement);
 
-    permissionButton.addEventListener('click', () => {
-        DeviceMotionEvent.requestPermission().then(response => {
-            if (response == 'granted') {
-                // Now we can enable the gimbal!
-                gimbal.enable();
-            }
-        });
-    });
+  const loader = new THREE.GLTFLoader();
+  loader.load('path/to/your/gltf/model.gltf', (gltf) => {
+    gltfModel = gltf.scene;
+    scene.add(gltfModel);
+  });
 
-    // Render loop
-    function render() {
-        // Performs all necessary calculations
-        gimbal.update();
+  // Handle gyroscope and accelerometer input
+  if (window.DeviceOrientationEvent) {
+    window.addEventListener('deviceorientation', handleDeviceOrientation, true);
+    console.log('DeviceOrientationEvent is supported.');
+  } else {
+    console.log('DeviceOrientationEvent is not supported.');
+  }
 
-        // Gets yaw rotation (y-axis)
-        // Range [-180, 180], 0 is forward
-        const yaw = gimbal.yaw;
-
-        // Gets pitch rotation (x-axis)
-        // Range [-180, 180], 0 is horizontal
-        const pitch = gimbal.pitch;
-
-        // Gets roll rotation (z-axis)
-        // Range [-180, 180], 0 is vertical
-        const roll = gimbal.roll;
-
-        // Update the camera's rotation based on gimbal data
-        camera.rotation.set(THREE.MathUtils.degToRad(pitch), THREE.MathUtils.degToRad(yaw), THREE.MathUtils.degToRad(roll));
-
-        // Render the scene with the updated camera angle
-        renderer.render(scene, camera);
-
-        requestAnimationFrame(render);
-    }
-
-    // Start the render loop
-    render();
+  window.addEventListener('resize', onWindowResize, false);
 }
 
-// Wait for the DOM to load before initializing the app
-document.addEventListener('DOMContentLoaded', init);
+function handleDeviceOrientation(event) {
+  const { alpha, beta } = event;
+
+  // Convert beta and alpha angles to radians.
+  const betaRad = THREE.MathUtils.degToRad(beta);
+  const alphaRad = THREE.MathUtils.degToRad(alpha);
+
+  // Calculate the new camera position based on the device orientation.
+  const cameraDistance = 5; // The distance of the camera from the center of the scene.
+  const cameraX = cameraDistance * Math.cos(betaRad) * Math.sin(alphaRad);
+  const cameraY = cameraDistance * Math.sin(betaRad);
+  const cameraZ = cameraDistance * Math.cos(betaRad) * Math.cos(alphaRad);
+
+  // Set the new camera position.
+  camera.position.set(cameraX, cameraY, cameraZ);
+
+  // Point the camera at the center of the scene.
+  camera.lookAt(0, 0, 0);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+init();
+animate();
